f8deee337c44af2c8733672cc6804b4cfd5f2edc
Eka Nova <pramudya.eka@nolimit.id>
2024-07-11 09:45:12 +0700

    chore: Add comment documentation for explain code function

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -13,72 +13,100 @@ import 'register_screen.dart';
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
   static const routeName = '/login';
-
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-
-  late bool _obscureText = true;
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-
+  // declare the assetsRive variable and assign
+  // the path to the animated_login_character.riv file
   var assetsRive = 'assets/animated_login_character.riv';
 
+  // declare the stateMachineController variable
+  //and Artboard variable
   StateMachineController? stateMachineController;
   Artboard? artboard;
 
+  // declare SMI to handle the animation state
+  // based properties on Rive website before
   SMITrigger? failTrigger, successTrigger;
   SMIBool? isHandsUp, isChecking;
   SMINumber? lookNum;
 
+  final _formKey = GlobalKey<FormState>();
+
+  late final bool _obscureText = true;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   void initState() {
     rootBundle.load(assetsRive).then((value) {
-      final file = RiveFile.import(value);
-      final art = file.mainArtboard;
-      stateMachineController =
-          StateMachineController.fromArtboard(art, "Login Machine");
+      // Load Rive asset file
+      final file = RiveFile.import(value); // Import the loaded file
+      final art = file.mainArtboard; // Get the main artboard
+      stateMachineController = StateMachineController.fromArtboard(
+          art, "interactionMachine"); // Create state machine controller
 
       if (stateMachineController != null) {
-        art.addController(stateMachineController!);
+        art.addController(
+            stateMachineController!); // Add controller to artboard
 
         for (var element in stateMachineController!.inputs) {
           if (element.name == "isChecking") {
-            isChecking = element as SMIBool;
+            isChecking = element as SMIBool; // Map checking status control
           } else if (element.name == "isHandsUp") {
-            isHandsUp = element as SMIBool;
+            isHandsUp = element as SMIBool; // Map hands-up animation control
           } else if (element.name == "trigSuccess") {
-            successTrigger = element as SMITrigger;
+            successTrigger = element as SMITrigger; // Map success trigger
           } else if (element.name == "trigFail") {
-            failTrigger = element as SMITrigger;
+            failTrigger = element as SMITrigger; // Map failure trigger
           } else if (element.name == "numLook") {
-            lookNum = element as SMINumber;
+            lookNum = element as SMINumber; // Map gaze direction control
           }
         }
       }
-      setState(() => artboard = art);
+      setState(() => artboard = art); // Update UI with prepared animation
     });
     super.initState();
   }
 
+  // This method sets the animation
+  // to a "looking around" state.
+  // It activates when users type in the email field
   void lookAround() {
     isChecking?.change(true);
     isHandsUp?.change(false);
     lookNum?.change(0);
   }
 
+  // This method adjusts the character's
+  // gaze based on input length
+  // It's likely used to make the character
+  // look at the text being entered
   void moveEyes(value) {
     lookNum?.change(value.length.toDouble());
   }
 
+  // This method sets the animation to
+  // a "hands up on eyes" state
+  // It activates when users type
+  // in the password field
   void handsUpOnEyes() {
     isHandsUp?.change(true);
     isChecking?.change(false);
   }
 
+  // This method deactivates all animations
+  // It's likely used to reset the character's state
+  void handsDown() {
+    isHandsUp?.change(false);
+    isChecking?.change(false);
+  }
+
+  // This method triggers the animation
+  // for an incorrect input or failed state
+  // It deactivates ongoing animations,
   void wrongState() {
     isChecking?.change(false);
     isHandsUp?.change(false);
@@ -86,6 +114,9 @@ class _LoginScreenState extends State<LoginScreen> {
     failTrigger?.fire();
   }
 
+  // This method triggers the animation for
+  // a correct input or successful state
+  // It deactivates ongoing animations,
   void successState() {
     isChecking?.change(false);
     isHandsUp?.change(false);
@@ -130,7 +161,6 @@ class _LoginScreenState extends State<LoginScreen> {
     Widget formLogin() {
       return Container(
         margin: EdgeInsets.only(
-          // top: 30,
           left: defaultMargin,
           right: defaultMargin,
         ),
@@ -141,7 +171,9 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               CustomTextFormField(
                 label: 'Email',
+                // Triggers "look around" animation when email field is tapped
                 onTap: lookAround,
+                // Moves character's eyes based on input length
                 onChanged: ((value) => moveEyes(value)),
                 controller: _emailController,
                 validator: (validator) {
@@ -157,76 +189,77 @@ class _LoginScreenState extends State<LoginScreen> {
               const SizedBox(
                 height: 16,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  CustomTextFormField(
-                    label: 'Password',
-                    onTap: handsUpOnEyes,
-                    controller: _passwordController,
-                    isPassword: _obscureText,
-                    onChanged: (p0) {
-                      handsUpOnEyes();
-                    },
-                    validator: (validator) {
-                      if (validator!.isEmpty) {
-                        return 'Password is required';
-                      } else {
-                        final password = validator;
-                        if (!password.hasMinLength(8)) {
-                          return 'Password must be at least 8 characters';
-                        } else {
-                          return null;
-                        }
-                      }
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  CustomButtonWidget(
-                    btnName: 'Login',
-                    width: defaultMargin,
-                    statusColor: true,
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        String email = "email@mail.com";
-                        String password = "pass1234";
+              CustomTextFormField(
+                label: 'Password',
+                // Triggers "hands up on eyes" animation when password field is tapped
+                onTap: handsUpOnEyes,
+                controller: _passwordController,
+                isPassword: _obscureText,
+                onChanged: (p0) {
+                  // Triggers "hands up on eyes" animation when password field is changed
+                  handsUpOnEyes();
+                },
+                validator: (validator) {
+                  if (validator!.isEmpty) {
+                    return 'Password is required';
+                  } else {
+                    final password = validator;
+                    if (!password.hasMinLength(8)) {
+                      return 'Password must be at least 8 characters';
+                    } else {
+                      return null;
+                    }
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CustomButtonWidget(
+                btnName: 'Login',
+                width: defaultMargin,
+                statusColor: true,
+                onPressed: () {
+                  // Resets character to neutral state when login button is pressed
+                  handsDown();
+                  if (_formKey.currentState!.validate()) {
+                    String email = "email@mail.com";
+                    String password = "pass1234";
 
-                        if (_emailController.text == email &&
-                            _passwordController.text == password) {
-                          successState();
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              backgroundColor: Colors.green,
-                              content: Text('Login Success'),
-                            ),
-                          );
-                        } else {
-                          wrongState();
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              backgroundColor: Colors.red,
-                              content: Text('Login Failed'),
-                            ),
-                          );
-                        }
-                      }
-                    },
+                    if (_emailController.text == email &&
+                        _passwordController.text == password) {
+                      // Triggers success animation if login is correct
+                      successState();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          backgroundColor: Colors.green,
+                          content: Text('Login Success'),
+                        ),
+                      );
+                    } else {
+                      // Triggers failure animation if login is incorrect
+                      wrongState();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          backgroundColor: Colors.red,
+                          content: Text('Login Failed'),
+                        ),
+                      );
+                    }
+                  }
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  'Forgot Password?',
+                  style: txMedium.copyWith(
+                    color: primaryColor,
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Text(
-                      'Forgot Password?',
-                      style: txMedium.copyWith(
-                        color: primaryColor,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
               const SizedBox(
                 height: 45,
